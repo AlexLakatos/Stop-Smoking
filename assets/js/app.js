0e545b1d3e8364efddcd2cbb7b94159a65c4db4c
Alex Lakatos <alex@greensqr.com>
2012-03-01 18:41:36 +0200

    added the messages and the Options button

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,28 @@ function populateData(flag) {
     resetDays();
     populateDays(flag);
   }
-  else { 
-    document.getElementById(flag + "-quote").textContent = no[0];
-    increaseDays(1);
-    populateDays(flag);
+  else {
+    if (flag === 'no') {
+	  document.getElementById(flag + "-quote").textContent = no[0];
+      increaseDays(1);
+      populateDays(flag);
+	  } else {
+	  populateDays(flag);
+	  }
   }
 }
 
 function populateDays(flag) {
-  document.getElementById("days").textContent = getDays();
+  if (getDays() == 0) {
+	document.getElementById("days").textContent = "FAIL";
+	} else {
+	  document.getElementById("days").textContent = getDays();
+	  } 
   if (flag !== '') {
-    document.getElementById(flag + "-days").textContent = getDays();
+    if (getDays() == 0) {
+	  document.getElementById(flag + "-days").textContent = "FAIL";
+	  } else {
+	    document.getElementById(flag + "-days").textContent = getDays();
+		}
   }
 }
